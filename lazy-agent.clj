d18166627eaa60e46321e8ef2fe09800c66b25ee
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-02-28 00:45:03 +0000

    Tweaks

--- lazy-agent.clj
+++ lazy-agent.clj
@@ -1,6 +1,7 @@
 ; Author: Anand Patil
 ; Date: Feb 5, 2009
-; Common Public License
+; Creative Commons BY-SA, see LICENSE
+; copyright 2009 Anand Patil
 
 ; Good reference on scheduling: Scheduling and Automatic Parallelization.
 ; Chapter 1 covers scheduling in DAGs and is available free on Google Books.
@@ -8,22 +9,24 @@
 (set! *warn-on-reflection* true)
 (ns lazy-agent)
 
-
 ; ==================================================
 ; = Utility stuff not immediately related to cells =
 ; ==================================================
 (defn agent? [x] (instance? clojure.lang.Agent x))
 (defn id? [x] (instance? clojure.lang.IDeref x))
 (defn deref-or-val [x] (if (id? x) @x x))
-
 (defn map-now [fn coll] (doall (map fn coll)))
 (defn second-arg [x y] y)
 (defn set-agent! [a v] (send a second-arg v))
 
-
 ; ==================
 ; = Updating stuff =
 ; ==================
+
+(defn updating-fn [x] (if (:needs-update x) (assoc x :updating true) x))
+(defn force-need-update-fn [x] {:needs-update true})
+(defn send-force-need-update [p] (send p force-need-update-fn))
+(defn send-update [p] "Utility function that puts p into the updating state." (send p updating-fn))
 
 (defn complete-parents [val parents]
     "Takes a map of the form {parent @parent}, and a list of mutable and
@@ -31,16 +34,15 @@
     correct order."
     (loop [parents-sofar parents val-sofar (list)]
         (if (empty? parents-sofar) val-sofar
-            (let [
-            parent (last parents-sofar) 
-            rest-parents (butlast parents-sofar) 
-            this-val (val parent)
-            ]
-            (if this-val
-                ; If value has a key corresponding to this parent, cons the corresponding value
-                (recur rest-parents (cons this-val val-sofar))
-                ; Otherwise, cons the parent.
-                (recur rest-parents (cons (deref-or-val parent) val-sofar)))))))
+            (let [parent (last parents-sofar) 
+                rest-parents (butlast parents-sofar) 
+                this-val (val parent)
+                ]
+                (if this-val
+                    ; If value has a key corresponding to this parent, cons the corresponding value
+                    (recur rest-parents (cons this-val val-sofar))
+                    ; Otherwise, cons the parent.
+                    (recur rest-parents (cons (deref-or-val parent) val-sofar)))))))
 
 (defn swap-id-parent-value [val parent parent-val]
     "Utility function that incorporates updated parents into a cell's
@@ -49,14 +51,9 @@
         (dissoc val parent)
         (assoc val parent parent-val)))
 
-(defn updating-fn [x] (if (:needs-update x) (assoc x :updating true) x))
-(defn force-need-update-fn [x] {:needs-update true})
-(defn send-force-need-update [p] (send p force-need-update-fn))
-(defn send-update [p] "Utility function that puts p into the updating state." (send p updating-fn))
-
 (defn compute [parents agent-parent-vals update-fn] 
     "Utility function that applies a cell's updating function to its
-    parents." 
+    parents."
     (apply update-fn (complete-parents @agent-parent-vals parents)))
 
 (defn report-to-child [val parent parent-val id-parents parents update-fn id-parent-vals oblivious?]
@@ -90,12 +87,12 @@
             (report-to-child cell-val p p-val id-parents parents update-fn id-parent-vals oblivious?)
             cell-val)))
 
-(defn cell-watcher [cell id-parents id-parent-vals agent-parents parents update-fn]    
+(defn cell-watch [cell id-parents id-parent-vals agent-parents parents update-fn]    
     "Watches a non-root cell. If it changes and requests an update,
     it computes if possible. Otherwise it sends an update request to all its
     parents."
     (let [compute-fn (fn [junk] (compute parents id-parent-vals update-fn))]
-        (fn cell-watcher [key cell old-val cell-val]
+        (fn [key cell old-val cell-val]
             (if (not= old-val cell-val)
                 (if (:updating cell-val)
                     (if (= (count @id-parent-vals) (count id-parents))
@@ -120,7 +117,7 @@
         (do
             ; Add a watcher to all the cell's parents            
             (map-now add-parent-watcher id-parents)
-            (add-watch cell :key (cell-watcher cell id-parents id-parent-vals agent-parents parents update-fn))
+            (add-watch cell :self (cell-watch cell id-parents id-parent-vals agent-parents parents update-fn))
             cell)))
 
 (defmacro def-cell
